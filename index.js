d106290d2dde1f2ef4d841e6504a0e2460c1a96c
Dan Hughes <dan.hughes@heathwallace.com>
2017-09-14 10:03:14 +0100

    Trim env variables and remove email

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ require('dotenv').config();
 // Load environment variables
 assert(process.env.FORECAST_ACCOUNT_ID, 'FORECAST_ACCOUNT_ID environment variable is unset');
 assert(process.env.FORECAST_AUTHORIZATION, 'FORECAST_AUTHORIZATION environment variable is unset');
-assert(process.env.EMAIL, 'EMAIL environment variable is unset');
-const accountId = process.env.FORECAST_ACCOUNT_ID;
-const authorization = process.env.FORECAST_AUTHORIZATION;
-const email = process.env.EMAIL;
+const accountId = process.env.FORECAST_ACCOUNT_ID.trim();
+const authorization = process.env.FORECAST_AUTHORIZATION.trim();
 
 // Configure output handler, default to console if pushbullet not configured
 const apiKey = process.env.PUSHBULLET_API_KEY;
-let output = apiKey ? require('./pushbullet')(apiKey) : console.log.bind(console);
+let output = apiKey ? require('./pushbullet')(apiKey.trim()) : console.log.bind(console);
 
 // Connect API to Forecast
 const forecast = new Forecast({ accountId, authorization });
