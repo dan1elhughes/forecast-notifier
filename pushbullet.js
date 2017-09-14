50410b0c0556ff55826cc73dff8161d140e96dc7
Dan Hughes <dan.hughes@heathwallace.com>
2017-09-14 10:05:03 +0100

    Adds tags and promises to Pushbullet

--- pushbullet.js
+++ pushbullet.js
@@ -6,25 +6,27 @@ const dateformat = require('dateformat');
 const format = date => dateformat(date, 'mmmm dS');
 const endpoint = s => `https://api.pushbullet.com/v2${s}`;
 
-module.exports = apiKey => projects => {
+const toReadableDate = (a, b) => a === b ? '' : `until ${format(b)}`;
+const toReadableTags = tags => tags.length ? `(${tags.join(', ')})` : '';
+
+module.exports = apiKey => projects => new Promise((resolve, reject) => {
+
 	const headers = { 'Access-Token': apiKey };
 
 	const url = endpoint('/pushes');
 
 	const type = 'note';
 	const title = 'Today\'s projects';
-	const body = projects.map(_ => `${_.name} (${format(_.start)} to ${format(_.end)})`).join('\n');
+	const body = projects.map(_ => `${_.name} ${toReadableDate(_.start, _.end)} ${toReadableTags(_.tags)}`).join('\n');
 
 	const form = { type, title, body };
 
 	request.post({ headers, url, form }, (err, response, body) => {
 		if (response.statusCode === 200) {
 			console.log('Sent notification');
-			process.exit(0);
+			return resolve();
 		} else {
-			console.error(err);
-			console.error(body);
-			process.exit(1);
+			return reject(['pushbullet', err, body]);
 		}
 	});
-};
+});
